8205da53803ed767b4e6bd5c29bca5b81c7a993e
Jagannath Nayak <jagannath08108nayak@gmail.com>
2025-07-05 19:12:45 +0530

    Binary Search Question

--- DSA/Searching/Binary_Search.cpp
+++ DSA/Searching/Binary_Search.cpp
@@ -7,7 +7,7 @@ int getQuotient(int dividend, int divisor){
     // search space -> (-ve dividend) to (+ve dividend)
     int s = 0;
     int e = dividend;
-    int mid = s + ((e-s)>>2);
+    int mid = s + ((e-s)>>1);
     int ans = -1;
     while(s<=e){
         if((divisor*mid)==dividend) return mid;
@@ -17,19 +17,19 @@ int getQuotient(int dividend, int divisor){
             s = mid + 1;
         }
         else e = mid - 1;
-        mid = s + ((e-s)>>2);
+        mid = s + ((e-s)>>1);
     }
     return ans;
 
 }
 
 int main(){
-    int dividend = 15;
+    int dividend = 35;
     int divisor = 4;
     // Mission is to find quotient
     int ans = getQuotient(abs(dividend), abs(divisor));
     if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) ans = 0 - ans;
     cout << "Final ans: " << ans << endl;
-    
+
     return 0;
 }
