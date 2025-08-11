2c83bb408f8f58d9fb10ff109fdd4e2aacd57e5a
Jagannath Nayak <jagannath08108nayak@gmail.com>
2025-08-11 12:25:24 +0530

    RE

--- DSA/Recursion/code5.cpp
+++ DSA/Recursion/code5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
 void LTRRE(string& s, char x, int i, int& ans){
@@ -32,9 +33,42 @@ void ReverseRE(string& s, int start, int end){
     ReverseRE(s, start+1, end-1);
 }
 
+bool isPalindrome(string& s, int i, int j){
+    // Base case
+    if(i >= j) return true;
+
+    // Ek case mai solve karunga 
+    if(s[i] != s[j]) return false;
+
+    // Baaki RE dekh lega
+    return isPalindrome(s, i+1, j-1);
+}
+
+void PrintsubArray_util(vector<int>& nums, int start, int end){
+    // Base case
+    if(end == nums.size()) return;
+
+    // Ek case
+    for(int i=start; i<=end; i++){
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    // RE
+    PrintsubArray_util(nums, start, end+1);
+
+}
+
+void PrintsubArray(vector<int>& nums){
+    for(int start = 0; start<nums.size(); start++){
+        int end = start;
+        PrintsubArray_util(nums, start, end);
+    }
+}
+
 int main(){
-    string s;
-    cin>>s;
+    // string s;
+    // cin>>s;
     // char x;
     // cin>>x;
     // int ans = -1;
@@ -43,10 +77,13 @@ int main(){
     // RTLRE(s, x, s.size()-1, ans);
     // cout << ans << endl;
 
-    ReverseRE(s, 0, s.size()-1);
-    cout << s << endl;
+    // ReverseRE(s, 0, s.size()-1);
+    // cout << s << endl;
+    
+    // cout << isPalindrome(s, 0, s.size()-1) << endl;
 
-
+    vector<int> nums = {1,2,3,4,5};
+    PrintsubArray(nums);
 
 
     return 0;
