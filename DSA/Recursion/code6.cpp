ee0c24e2d197ad66d29aad9cd0d53733fc10066d
Jagannath Nayak <jagannath08108nayak@gmail.com>
2025-08-17 12:10:06 +0530

    RE

--- DSA/Recursion/code6.cpp
+++ DSA/Recursion/code6.cpp
@@ -10,9 +10,23 @@ int solve(int n){
     return ans;
 }
 
+int PaintWays(int n, int k){
+    // Base case
+    if(n==1) return k;
+    if(n==2) return k + k*(k-1);
+
+    int ans = (k-1) * (PaintWays(n-2,k)+PaintWays(n-1,k));
+    return ans;
+}
+
 int main(){
+    // int n = 3;
+    // cout << solve(n) << endl;
+
     int n = 3;
-    cout << solve(n) << endl;
+    int k = 3;
+    int ans = PaintWays(n,k);
+    cout << ans << endl;
 
 
     return 0;
