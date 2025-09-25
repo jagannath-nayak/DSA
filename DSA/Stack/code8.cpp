f99ca3b1e5a89564657e30b874c81aff5fad7f71
Jagannath Nayak <jagannath08108nayak@gmail.com>
2025-09-25 15:45:28 +0530

    stack

--- DSA/Stack/code8.cpp
+++ DSA/Stack/code8.cpp
@@ -4,9 +4,23 @@
 #include<algorithm>
 using namespace std;
 
-void solve(vector<int>& v, stack<int>& s, vector<int>& ans){
+void nextSmallerElement(vector<int>& v, stack<int>& s, vector<int>& ans){
     int n = v.size();
     for(int i=n-1; i>=0; i--){
+        int element = v[i];
+        while(s.top() > element){
+            s.pop();
+        }
+        if(s.top() < element){
+            ans.push_back(s.top());
+            s.push(element);
+        }
+    }
+}
+
+void prevSmallerElement(vector<int>& v, stack<int>& s, vector<int>& ans){
+    int n = v.size();
+    for(int i=0; i<n; i++){
         int element = v[i];
         while(s.top() > element){
             s.pop();
@@ -30,7 +44,8 @@ int main(){
     stack<int> s;
     s.push(-1);
     vector<int> ans;
-    solve(v, s, ans);
+    //nextSmallerElement(v, s, ans);
+    prevSmallerElement(v, s, ans);
     reverse(ans.begin(), ans.end());
     for(int i=0; i<ans.size(); i++){
         cout << ans[i] << " ";
